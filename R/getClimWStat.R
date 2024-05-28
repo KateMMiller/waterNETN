39ac3e91f45c2a25c1915fdec548fe20917c5744
KateMMiller <kathryn_miller@nps.gov>
2024-05-28 13:55:44 -0400

    fixed reduce for multiple parks

--- R/getClimWStat.R
+++ R/getClimWStat.R
@@ -183,7 +183,7 @@ getClimWStat <- function(park = "all", site = "all",
                          park = parkc)
 
     ws_ls <- list(pcp_in, tmax_f, tmin_f)
-    ws_comb <- reduce(ws_ls, full_join, by = c("SiteCode", "UnitCode", "Date"))
+    ws_comb <- reduce(ws_ls, full_join, by = c("UnitCode", "Date"))
     } else {
     tmax_f <- get_wdat('maxt',
                        stn = station,
@@ -193,7 +193,7 @@ getClimWStat <- function(park = "all", site = "all",
                        park = parkc)
 
     ws_ls <- list(tmax_f, tmin_f)
-    ws_comb <- purrr::reduce(ws_ls, full_join, by = c("SiteCode", "UnitCode", "Date"))
+    ws_comb <- purrr::reduce(ws_ls, full_join, by = c("UnitCode", "Date"))
     ws_comb$pcp_in <- NA_real_
     }
   })
