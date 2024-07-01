1ee06622ca84728426d0a26106ee1096c2f2193c
KateMMiller <kathryn_miller@nps.gov>
2024-07-01 07:40:41 -0400

    dropped Bowl and SargMtn Pond from active list

--- R/getSites.R
+++ R/getSites.R
@@ -120,7 +120,8 @@ getSites <- function(park = "all", site = "all", site_type = c("all", "lake", "s
     }
 
   # filter on active. Currently hard coded until data package includes it
-  inactive = c("ACBUBO", "ACEGLO", "ACJRDO", "ACMOWB", "MORRSA", "MORRSC", "ROVASC", "SARASB", #streams
+  inactive = c("ACBUBO", "ACEGLO", "ACJRDO", "ACMOWB", "ACBOWL", "ACSAMP",
+               "MORRSA", "MORRSC", "ROVASC", "SARASB", #streams
                "ACDKPD", "ACLPIH", "ACTARN", "ROVAPA", "SAGAPA") # lakes
   wdata3 <-
   if(active == TRUE){
