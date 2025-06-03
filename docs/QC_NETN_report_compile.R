d0800d396c55320c0ebefcc514edfc61985949fa
KateMMiller <kathryn_miller@nps.gov>
2025-06-03 15:29:17 -0400

    more checks

--- docs/QC_NETN_report_compile.R
+++ docs/QC_NETN_report_compile.R
@@ -58,20 +58,23 @@ QC_table <- pct_check(param = "ANC_ueqL", meas_type = 'Water Quality', tab = 'La
 lab_check <- QC_table |> filter(Data %in% "Lab Measurements" & Num_Records > 0)
 lab_include <- tab_include(lab_check)
 
-
 #------ WQual: Lab vs Sonde Parameters -------
 QC_table <- svl_pct_check(param_sonde = "pH", param_lab = "pH_Lab")
 
 # Output of svl_pct_check: tbl_pH_10 and pctdiff_pH
 
-
-# Then make this into a function to do multiple params.
+#+++ Add lines for remaining parameters comps. Just need to change the param name and assign it to QC_table.
+#+++ For each line above a kable and a figure are returned (eg tbl_pH_10, pctdiff_pH). Those
+#+++ need to be added to the QC_NETN_report in the Lab cs Sonde tab.
 
 # check if Lab v Sond checks returned at least 1 record to determine whether to include that tab in report
 lab_v_sonde_check <- QC_table |> filter(Data %in% "Lab vs Sonde" & Num_Records > 0)
-lab_v_sond_include <- tab_include(lab_v_sond_check)
+lab_v_sonde_include <- tab_include(lab_v_sonde_check)
 
 #------ WQual: DO 899 & 999 checks -------
+
+do <- getSondeInSitu(years = year_range, parameter = "DOsat_pct", QC_type = 'all')
+table(do$QCtype)
 
 # check if DO 899/999 checks returned at least 1 record to determine whether to include that tab in report
 do899_check <- QC_table |> filter(Data %in% "DO 899 and 999" & Num_Records > 0)
@@ -90,7 +93,7 @@ blank_check <- QC_table |> filter(Data %in% "BLANK Samples" & Num_Records > 0)
 blank_include <- tab_include(blank_check)
 
 #------ WQual: Secchi Depth -------
-
+QC_table <- pct_check(param = "SDepth_m", meas_type = 'Water Quality', tab = 'Secchi Depth', chk_type = "check")
 
 # check if Secchi checks returned at least 1 record to determine whether to include that tab in report
 secchi_check <- QC_table |> filter(Data %in% "Secchi Depth" & Num_Records > 0)
@@ -101,24 +104,30 @@ wqual_check <- QC_table |> filter(Type %in% "Water Quality" & Num_Records > 0)
 wqual_include <- tab_include(wqual_check)
 
 #---- Water Quantity Checks ----
+QC_table <- pct_check(param = "WaterLevel_m", meas_type = 'Water Quantity', tab = 'Lake Level', chk_type = "check")
+QC_table <- pct_check(param = "Discharge_cfs", meas_type = 'Water Quantity', tab = 'Lake Level', chk_type = "check")
 
-#------ WQuant: Lake Level -------
+# check if Lake level checks returned at least 1 record to determine whether to include that tab in report
+lakelev_check <- QC_table |> filter(Data %in% "Lake Level" & Num_Records > 0)
+lakelev_include <- tab_include(lakelev_check)
 
-#------ WQuant: Discharge -------
 
+# check if discharge checks returned at least 1 record to determine whether to include that tab in report
+disch_check <- QC_table |> filter(Data %in% "Discharge" & Num_Records > 0)
+disch_include <- tab_include(disch_check)
 
 # check if Water Quantity checks returned at least 1 record to determine whether to include that tab in report
 wquant_check <- QC_table |> filter(Type %in% "Water Quantity" & Num_Records > 0)
 wquant_include <- tab_include(wquant_check)
 
 #+++++ Compile final QC Table
-#+# revise for different color combos for checks (99 vs 90)
+#+# revise for different color combos for checks (99 vs 90)?
 QC_check_table <- kable(QC_table, format = 'html', align = 'c', caption = "QC checking results",
                          col.names = c("Type", "Data Tab", "Check Description", "Number of Records", "Check Type")) |>
   kable_styling(fixed_thead = TRUE, bootstrap_options = c('condensed'),
                 full_width = TRUE, position = 'left', font_size = 12) |>
   row_spec(0, extra_css = "border-top: 1px solid #000000; border-bottom: 1px solid #000000;") |>
-  column_spec(3, width = "150px") |>
+  column_spec(3, width = "300px") |>
   column_spec(2:ncol(QC_table), background =
                 ifelse(QC_table$Num_Records > 0 & QC_table$check_type == "error", "#F2F2A0",
                        ifelse(QC_table$Num_Records > 0 & QC_table$check_type == "check", "#b7d8ef", "#ffffff"))) |>
